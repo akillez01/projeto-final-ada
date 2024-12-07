5f4a01dad5429ce6a3a94174364c50fe83f78455
akillez01 <achilles.oliveira.souza@gmail.com>
2024-12-07 18:13:22 -0500

    feat: atualização do s3

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,10 +1,17 @@
 resource "aws_s3_bucket" "file_bucket" {
   bucket = var.bucket_name
 
-  # Adiciona uma regra de ciclo de vida para esvaziar o bucket antes de excluir
-  lifecycle_rule {
-    enabled = true
-    prefix  = "/"
+  lifecycle {
+    ignore_changes = [bucket]
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "file_bucket_lifecycle" {
+  bucket = aws_s3_bucket.file_bucket.bucket
+
+  rule {
+    id     = "expire-objects"
+    status = "Enabled"
 
     expiration {
       days = 1
