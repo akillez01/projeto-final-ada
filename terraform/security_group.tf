d6ef079df02b3b753774272ac73b406518e3453f
akillez01 <achilles.oliveira.souza@gmail.com>
2024-12-08 17:21:23 -0500

    Corrigir política do bucket S3, caminho do arquivo arquivolambda.zip e verificar recursos existentes

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -1,31 +1,26 @@
-# Busca a VPC padrão da conta
-data "aws_vpc" "default" {
-  default = true
-}
-
-# Criação do Security Group
 resource "aws_security_group" "default" {
   name        = "default_security_group"
   description = "Default security group"
-  vpc_id      = data.aws_vpc.default.id # Usando a VPC padrão
+  vpc_id      = var.vpc_id
 
-  # Regras de entrada (Ingress)
+  lifecycle {
+    ignore_changes = [name]
+  }
+
   ingress {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Permite acesso de qualquer IP (recomendado verificar se é adequado)
-  }
-
-  # Regras de saída (Egress)
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"  # Permite todo tipo de tráfego de saída
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Tags para identificação
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "default_security_group"
   }
